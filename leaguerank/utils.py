f5cf238b4a1830512eedbda157d84476e465f35d
David Horn <david@d8a.me>
2018-07-17 16:10:25 -0700

    Fixed bugs

--- leaguerank/utils.py
+++ leaguerank/utils.py
@@ -1,12 +1,19 @@
 import json
 from urllib import urlencode
 import urllib2
-from time import strptime
+from datetime import datetime
 
 
 def getJSON(url, headers={}, params={}):
+    if params is None:
+        params = {}
+    params = urlencode(params)
+    if url.find('?') > 0 and len(params):
+        url = url + '&' + params
+    elif len(params):
+        url = url + '?' + params
     request = urllib2.Request(
-        url + '?' + urlencode(params), 
+        url, 
         headers=headers)
     response = urllib2.urlopen(request)
     return json.load(response)
@@ -14,10 +21,10 @@ def getJSON(url, headers={}, params={}):
 def postJSON(url, headers={}, data={}):
     if data is None:
         data = {}
-    request = urllib2.Request(url, headers=headers, data=data)
+    request = urllib2.Request(url, headers=headers, data=urlencode(data))
     response = urllib2.urlopen(request)
     return json.load(response)
 
 def convert_iso_time(isotime):
     """Will convert an isotime (string) to datetime.datetime"""
-    return strptime(isotime, '%Y-%m-%dT%H:%M:%S.%fZ')
+    return datetime.strptime(isotime, '%Y-%m-%dT%H:%M:%S.%fZ')
