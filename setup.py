c704d8ce3724c5ef17843e5c1a0cc569f0c17113
David Horn <david@d8a.me>
2018-07-31 13:54:17 -0700

    revving version derp

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='python-fortniteapi-wrapper',
-    version='1.0.0',
+    version='1.0.1',
     description='A python Fortnite API wrapper that doesn\'t suck',
     long_description=long_description,
     long_description_content_type='text/markdown',
